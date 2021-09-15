43e2ba5265896b612151314c48d7c1f9b901df64
martinschobben <m.a.n.schobben@uu.nl>
2021-09-15 17:29:55 +0200

    use bookdown for html document

--- ghgenerate.R
+++ ghgenerate.R
@@ -2,7 +2,7 @@ proposal <- "ISC Proposal panacea"
 proposal.file <- "isc-proposal.Rmd"
 author <- "Martin Schobben"
 
-rmarkdown::render(proposal.file, output_format="html_document",
+rmarkdown::render(proposal.file, output_format="bookdown::html_document2",
                   output_dir="out", quiet=TRUE)
 rmarkdown::render(proposal.file, output_format="pdf_document",
                   output_dir="out", quiet=TRUE)
