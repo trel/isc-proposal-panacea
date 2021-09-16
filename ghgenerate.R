59226f3435af87c997dbc7fc22bc53e134063246
martinschobben <schobbenmartin@gmail.com>
2021-09-16 07:32:30 +0200

    bookdown for pdf

--- ghgenerate.R
+++ ghgenerate.R
@@ -4,5 +4,5 @@ author <- "Martin Schobben"
 
 rmarkdown::render(proposal.file, output_format="bookdown::html_document2",
                   output_dir="out", quiet=TRUE)
-rmarkdown::render(proposal.file, output_format="pdf_document",
+rmarkdown::render(proposal.file, output_format="bookdown::pdf_book",
                   output_dir="out", quiet=TRUE)
